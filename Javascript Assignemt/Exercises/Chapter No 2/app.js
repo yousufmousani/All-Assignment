c75bc11b32fc402e620f52916610e081bd80eed8
yousuf-mousani <khananirafiq@gmail.com>
2024-10-14 21:24:58 +0500

    All Assignment

---
+++ Javascript Assignemt/Exercises/Chapter No 2/app.js
@@ -0,0 +1,32 @@
+// var username = 'yousuf'
+
+// var myName = 'Yousuf Moosani'
+
+// var message = 'Hello World'
+
+// alert(message);
+
+// var data = 'Yousuf moosani'
+// var data1 = '19 years old'
+// var data2 = 'learn the web developnment course'
+// alert(data)
+// alert(data1)
+// alert(data2)\\
+
+
+// var data = ('PIZZA\nPIZZ\nPIZ\nPI\nP')
+// alert(data)
+
+// var email = 'My Email address is Yousufmousani@.com'
+// alert(email)
+
+// var book = 'Iam trying to learn from the Book A smarter way to learn Javascript'
+// alert(book)
+
+// var write = 'Yah! I can write HTML content through JavaScript'
+// document.write(write)
+
+// var write = 'Yah! I can write HTML content through JavaScript'
+// alert(write)
+// document.write(write)
+
