fedc225f18758eff87976e30f636ebd75ad47af2
yousuf-mousani <khananirafiq@gmail.com>
2024-10-15 15:45:56 +0500

    All Assignmenta

---
+++ Javascript Assignemt/Exercises/Chapter No 3/app.js
@@ -0,0 +1,27 @@
+// Q1
+
+// var age = 15
+// alert('I am'+' '+age+' years old')
+
+// Q2
+
+
+// var visit = 12
+// alert('You have visited this site'+' '+visit+' Times')
+
+// Q3 
+
+// var birth =2005
+// document.write('My Birth year is'+' '+birth + '<br>')
+// document.write('Data type of my declare variable is '+ typeof birth)
+
+// Q4
+
+// var welcome =   'Welcome to our Mousani Shop'
+// alert(welcome)
+
+// var user = prompt('Enter Your Name')
+// var Product = prompt('Enter Your Product Name')
+// var Quantity = prompt('Enter Product Quantity')
+
+// document.write(user+' '+'Ordered' +' '+ Quantity + ' '+ Product +' '+ 'On Mousani Shop')
