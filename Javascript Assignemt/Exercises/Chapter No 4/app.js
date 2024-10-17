5b67c5782f3bc9ea84e5d360eb0fdaff5c365355
yousuf-mousani <khananirafiq@gmail.com>
2024-10-17 18:37:47 +0500

    All Assignment

---
+++ Javascript Assignemt/Exercises/Chapter No 4/app.js
@@ -0,0 +1,25 @@
+// Q2 
+
+// Legal Variables
+
+// var user = ''
+// var $user = ''
+// var userName = ''
+// var user_name = ''
+// var user1 = ''
+
+// illegal Variables
+
+// var 1user =''
+// var user 1= ''
+// var 12 = ''
+
+
+var $ = '$'
+var _ = '_'
+var $name =  '$my_1Variable'
+var sensitive = 'sensitive'
+var keywords = 'keywords'
+
+document.write('Rules for naming JS variables'+ '<br>')
+document.write('Variables names can only contain'+' '+$+' '+'or'+_+ 'For example :'+$name+'<br/>')
