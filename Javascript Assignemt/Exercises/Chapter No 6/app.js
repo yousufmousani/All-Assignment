3da35e6b04c3fd135dc3bf8b443d892363a178d9
yousuf-mousani <khananirafiq@gmail.com>
2024-10-21 23:26:45 +0500

    All Assignment

---
+++ Javascript Assignemt/Exercises/Chapter No 6/app.js
@@ -0,0 +1,77 @@
+// Task 1
+
+// var a = 10
+
+// document.write('The value of a is : '+ a+'<br>')
+// document.write('---------------------------------------'+'<br>')
+
+// ++a 
+
+// document.write('The value of ++a is :' +a+'<br>')
+// document.write('Now the value of a is :' +a+'<br>')
+// document.write('---------------------------------------'+'<br>')
+// document.write('The value of a++ is :' +a+'<br>')
+
+// a++
+
+// document.write('Now the value of a++ is :' +a+'<br>')
+// document.write('---------------------------------------'+'<br>')
+
+// --a
+
+// document.write('The value of --a is :' +a+'<br>')
+// document.write('Now the value of --a is :' +a+'<br>')
+// document.write('---------------------------------------'+'<br>')
+// document.write('The value of --a is :' +a+'<br>')
+
+// a--
+
+
+// document.write('Now the value of --a is :' +a+'<br>')
+
+
+// Task 2
+
+// var a = 2
+// var b = 1
+
+// document.write('The value of a is : '+a+'<br>')
+// document.write('The value of b is : '+b+'<br>')
+// document.write('--a is : '+--a+'<br>')
+// document.write('--a - --b is : ' +(--a - --b)+'<br>')
+// document.write('--a - --b + ++b is :' +(--a - --b + ++b)+'<br>')
+// document.write('--a - --b + ++b +b-- is : '+ (--a - --b + ++b + b--)+'<br>')
+// document.write(a+'<br>')
+// document.write(b+'<br>')
+// document.write('Result'+(--a - --b + ++b + b--))
+
+// Task 3 
+
+// var Name = prompt('Enter Your Name')
+
+// document.write('Welcome'+' '+ Name)
+
+// Task 5
+
+// var table = prompt('Enter Table No',5)
+
+// document.write(table+' '+'x '+1 +' '+ '= '+  table*1+'<br>' )
+// document.write(table+' '+'x '+2 +' '+ '= '+  table*2+'<br>' )
+// document.write(table+' '+'x '+3 +' '+ '= '+  table*3+'<br>' )
+// document.write(table+' '+'x '+4 +' '+ '= '+  table*4+'<br>' )
+// document.write(table+' '+'x '+5 +' '+ '= '+  table*5+'<br>' )
+// document.write(table+' '+'x '+6 +' '+ '= '+  table*6 +'<br>')
+// document.write(table+' '+'x '+7 +' '+ '= '+  table*7+'<br>' )
+// document.write(table+' '+'x '+8 +' '+ '= '+  table*8+'<br>')
+// document.write(table+' '+'x '+9 +' '+ '= '+  table*9+'<br>')
+// document.write(table+' '+'x '+10 +' '+ '= '+  table*10 +'<br>')
+
+// Task 6
+
+// var English = +prompt('Enter the Obtain mark of English Subject')
+// var Math = +prompt('Enter the Obtain mark of Math Subject')
+// var Urdu = +prompt('Enter the Obtain mark of Urdu Subject')
+// var total = English+Math+Urdu
+
+// document.write('<table><tr><th>Subject<th> <th>Total Marks<th> <th>Obtained Marks<th> <th>Percentage<th><tr><br><tr><td>English<td> <td>100<td>' + '<td>'+English+'<td>'+'<td>'+English/100*100+'%'+'<td>'+'<br>'+'<tr><td>Math<td><td>100<td>'+'<td>'+Math+'<td>'+'<td>'+Math/100*100+'%<td><tr>'+'<tr><td>Urdu<td><td>100<td>'+'<td>'+Urdu+'<td>'+'<td>'+Urdu/100*100+'%'+'<td><tr>'+'<br>'+'<tr><th> <th> <th>300<th>'+'<th>'+total+'<th>'+'<th>'+ total/300*100+'%'+'<th><tr>')
+
