fedec8fd219ddfa540cccc1e741f40493651a095
yousuf-mousani <khananirafiq@gmail.com>
2024-10-21 17:34:13 +0500

    All Assignment

---
+++ Javascript Assignemt/Exercises/Chapter No 5/app.js
@@ -0,0 +1,101 @@
+// Task 1
+
+// var num1 = 12
+// var num2 = 6
+// var add = num1+num2
+// document.write('Addition '+num1+'+'+num2+' = '+add+'<br>')
+
+// Task 2 
+
+// var num1 = 12
+// var num2 = 6
+// var add = num1-num2
+// document.write('Substraction '+num1+'-'+num2+' = '+add+'<br>')
+
+// var num1 = 12
+// var num2 = 6
+// var add = num1*num2
+// document.write('Multiplication '+num1+'x'+num2+' = '+add+'<br>')
+
+// var num1 = 12
+// var num2 = 6
+// var add = num1/num2
+// document.write('Division '+num1+'/'+num2+' = '+add+'<br>')
+
+// var num1 = 12
+// var num2 = 6
+// var add = num1%num2
+// document.write('Modulus '+num1+'%'+num2+' = '+add+'<br>')
+
+// Task 3
+
+// var value = 5
+// document.write('Value is :'+value+'<br>')
+// document.write('value after Increment is :'+ ++value+'<br>')
+// var value2 = 7              
+// var value3 = value+value2
+// document.write('value after Addition is :'+value3+'<br>')
+// document.write('value after Decrement is :'+ --value3+'<br>')
+// document.write('The Reminder is :'+ (value%3)+'<br>')
+
+// Task 4
+
+// var ticket = 600
+// var quantity = 5
+// document.write('Total cost of buy '+quantity+' tickets to a Movie is    '+ticket*quantity+' PKR')
+
+// Task 5
+
+// var Table = prompt('Enter Table No')
+
+// document.write(Table+'x'+'1'+'='+Table*1+'<br>')
+// document.write(Table+'x'+'2'+'='+Table*2+'<br>')
+// document.write(Table+'x'+'3'+'='+Table*3+'<br>')
+// document.write(Table+'x'+'4'+'='+Table*4+'<br>')
+// document.write(Table+'x'+'5'+'='+Table*5+'<br>')
+// document.write(Table+'x'+'6'+'='+Table*6+'<br>')
+// document.write(Table+'x'+'7'+'='+Table*7+'<br>')
+// document.write(Table+'x'+'8'+'='+Table*8+'<br>')
+// document.write(Table+'x'+'9'+'='+Table*9+'<br>')
+// document.write(Table+'x'+'10'+'='+Table*10+'<br>')
+
+// Task 7
+
+// var price1 = 600
+// var delivery = 100
+// alert('T-shirt price is '+price1)
+// var quantity1 = prompt('Enter Quanitity of T-shirt')
+// document.write('Price of T-shirt is : '+price1+'<br>')
+// document.write('Quantity of T-shirt is : '+quantity1+'<br>')
+
+// var price2 = 100
+// alert('Price of Cap is '+price2)
+// var quantity2 = prompt('Enter the Quantity of Cap')
+// document.write('Price of Cap is : '+price2+'<br>')
+// document.write('Quamntity of Cap is : '+quantity2+'<br>')
+// document.write('Shipping charges is : '+delivery+'<br>')
+
+
+// var add = price1*quantity1+price2*quantity2+delivery+'<br>'
+// document.write('Total cost of Your order is '+add)
+
+// Task 8
+
+// var totalmarks = prompt('Enter Total Marks')
+// var Obtainmarks = prompt('Enter Obtain Marks')
+// var Result = Obtainmarks*100/totalmarks
+// document.write('Total Marks is '+totalmarks+'<br>')
+// document.write('Obtain Marka is '+Obtainmarks+'<br>')
+// document.write('Percentage is '+Result)
+
+// Task 8
+
+// alert('1 dollar is Equal to 104 Pakistani Rupees')
+// var dollar = prompt('Enter Dollor Quanity')
+
+// alert('1 Saudi Riyal is Equal to 28 Pakistani Rupess ')
+// var Riyal = prompt('Enter Saudi Riyal Quanity')
+
+// var add = dollar*104+Riyal*28
+
+// document.write('Total Currency in PKR is '+add)
