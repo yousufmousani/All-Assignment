f2dd9891c932f09940cfe23d3f568ff5fdd597f0
yousuf-mousani <khananirafiq@gmail.com>
2024-10-23 19:35:29 +0500

    All Assignment

---
+++ Javascript Assignemt/Exercises/Chapter No 9 to 11/task11.js
@@ -0,0 +1,17 @@
+var num1 = +prompt('Enter No 1')
+var num2 = +prompt('Enter No 2')
+var operater = prompt('Enter Operater \n +\n - \n * \n / \n %')
+
+if(operater == '+' ){
+alert(num1+num2)
+}else if(operater == '-' ){
+    alert(num1-num2)
+}else if(operater == '*' ){
+    alert(num1*num2)
+}else if(operater == '/' ){
+    alert(num1/num2)
+}else if(operater == '%' ){
+        alert(num1%num2)
+}
+
+
---
+++ Javascript Assignemt/Exercises/Chapter No 9 to 11/task8.js
@@ -0,0 +1,7 @@
+var user = prompt('Enter Number')
+
+if(user%2 != 0 ){
+    alert('Yes '+user+ ' can divide from 3')
+}else(
+    alert('No ' + user+' can"t divide from 3')
+)
