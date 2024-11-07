8014e6b383bf9faf2e3c74db087066beedb7dc57
yousuf-mousani <khananirafiq@gmail.com>
2024-11-07 22:42:03 +0500

    All Assignment

---
+++ Javascript Assignemt/Exercises/Chapter No 21-25/app.js
@@ -0,0 +1,50 @@
+// Task 1
+
+// var fisrtName = prompt('Enter First Name')
+// var LastName = prompt('Enter Last Name')
+// var fullName = fisrtName+' '+LastName
+// alert('Welcome '+fullName)
+
+// Task 2
+
+// var user = prompt('Enter Your favourite Mobile')
+// document.write('Your favourite Mobile is : '+user +'<br>'+'Length of string : '+ user.length)
+
+// Task 3
+
+// var Name = 'pakistani'
+// document.write('String :'+Name+'<br>'+'Index of n is : '+Name.indexOf('n'))
+
+// Task 4 
+// var Name = 'Hello World'
+// document.write('String :'+Name+'<br>'+'Last Index of l is : '+Name.lastIndexOf('l'))
+
+// Task5
+// var Name = 'Pakistani'
+// document.write('String : '+Name+'<br>'+'Character of index 3 : '+Name.charAt(3))
+
+// Task6
+// var fisrtName = prompt('Enter First Name')
+// var LastName = prompt('Enter Last Name')
+// var fullName = fisrtName.concat(LastName)
+// alert('Welcome '+fullName)
+
+// Task7
+
+// var Name = 'Hyderabad'
+// var replace = Name.replace('Hyder','Islam')
+// document.write('City : '+Name+'<br>'+'After replacement : '+replace)
+
+// Task8
+// var message = `Ali and Sami are best friends. They play cricket and football together.`
+// var replaceall = message.replaceAll('and','&')
+// document.write(replaceall)
+
+// Task9
+
+// var string = '472'
+// var change = Number(string)
+// document.write('Value '+string+'<br>'+'Type : '+typeof(string)+'<br>'+'Value '+string+'<br>'+'Type : '+typeof(change))
+
+
+
